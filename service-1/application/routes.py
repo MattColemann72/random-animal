87ce8943d223d9baa54cfed456e34455e6e816ba
MattColemann72 <82821511+MattColemann72@users.noreply.github.com>
2021-06-07 16:15:41 +0100

    Expanded to 3 services, all ready to get requested from service 1

--- service-1/application/routes.py
+++ service-1/application/routes.py
@@ -5,8 +5,8 @@ import random
 
 
 
-
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/', methods = ['GET', 'POST'])
+@app.route('/home', methods = ['GET', 'POST'])
 def index():
     randAnimal1 = random.randint(1,5)
     randAnimal2 = random.randint(6,10)
@@ -64,3 +64,38 @@ def index():
     # print(randanimal)
 
     return render_template('index.html', title="Random Animal Name Generator", randanimal=randanimal, animal2=animal2, animal1=animal1)
+
+
+# from flask import render_template, request, url_for, Response, jsonify, json
+# from application import app, db
+# from application.forms import GenerateForm
+# from application.models import characters
+# from sqlalchemy import desc
+# import random, requests, time
+
+
+# @app.route('/', methods = ['GET', 'POST'])
+# @app.route('/home', methods = ['GET', 'POST'])
+# def home():
+#     form = GenerateForm()
+#     if request.method == 'GET':
+#         return render_template('home.html', title='Class', form=form)
+#     if form.validate_on_submit():
+#         get_class = requests.get('http://service_two:5001/class')
+#         character_class = get_class.text
+#         get_race = requests.get('http://service_three:5002/race')
+#         character_race = get_race.text
+#         name = requests.post('http://service_four:5003/name', data=character_race)
+#         character_name = name.text
+#         stat = requests.post('http://service_four:5003/stats', data=character_race)
+#         stat_dict = json.loads(stat.text)
+#         data = characters(
+#                 name = character_name,
+#                 char_class = character_class,
+#                 char_race = character_race)
+#         db.session.add(data)
+#         db.session.commit()
+        
+#         stored_characters = characters.query.order_by(desc(characters.id)).limit(5).all()
+
+#         return render_template('home.html', title='Class', form=form, classes=character_class, race=character_race, name=character_name, stats=stat_dict, all_characters=stored_characters)
