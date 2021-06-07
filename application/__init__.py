7ef06de221b551cd09ea9923854c3bc2b6b16392
MattColemann72 <82821511+MattColemann72@users.noreply.github.com>
2021-06-07 14:13:12 +0100

    Really basic code complete.

--- application/__init__.py
+++ application/__init__.py
@@ -1,11 +1,9 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:crudapppass@34.105.192.173:3306/fantasyfootball'
-#'sqlite:///data.db' #'mysql+pymysql://root:crudapppass@host/fantasyfootball' DB location
-# app.config['SECRET_KEY'] = 'A SOOCRET KOO'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@externalip:port/dbname'
+#'sqlite:///data.db' # eg.. 'mysql+pymysql://root:crudapppass@host/fantasyfootball' DB location
+app.config['SECRET_KEY'] = 'SuPeRSecretK3y'
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# db = SQLAlchemy(app)
 
 from application import routes
