91eb2a94bca72b91734418309ed7d731c9676d43
mattc <you@example.com>
2021-06-08 16:27:04 +0000

    Almost all working - just need to fix the requests import and the app is done

--- service-1/application/routes.py
+++ service-1/application/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, request, jsonify, json
 from sqlalchemy import desc
-import random, requests
+import requests
 
 from application import app
 from application.forms import GenerateAnimal
@@ -15,29 +15,48 @@ def index():
     animal2 = "animal 2"
     randanimal = "new animal"
 
-    # animal1 = "Lion"
-    # animal2 = "dog"
-    # animal3 = "cat" 
-    # animal4 = "cow"
-    # animal5 = "sheep"
-    # animal6 = "hippopotamus"
-    # animal7 = "Elephant" 
-    # animal8 = "Monkey"
-    # animal9 = "Dolphin"
-    # animal10 = "Zebra"
-
     form = GenerateAnimal()
-
 
     if request.method == 'GET':
         return render_template('index.html', title="Random Animal Name Generator", form=form)
     
     if form.validate_on_submit():
-        get_animal1 = requests.get('http://service-2:5000/animal1')
+        get_animal1 = requests.get('http://service-2:5000/animal1').text
         animal1 = get_animal1.text
-        get_animal2 = requests.get('http://service-3:5000/animal2')
+        get_animal2 = requests.get('http://service-3:5000/animal2').text
         animal2 = get_animal2.text
 
         randanimal = animal1 + animal2
+        #Lion, Dog, Cat, Cow, Sheep
+        #"Li", "Do", "Ca", "Co", "She"
+        if animal1 == "Li":
+            animal1 = "Lion"
+        elif animal1 == "Do":
+            animal1 = "Dog"
+        elif animal1 == "Ca":
+            animal1 = "Cat"
+        elif animal1 == "Co":
+            animal1 = "Cow"
+        elif animal1 == "She":
+            animal1 = "Sheep"
+        
+        #hippopotamus,elephant,monkey,dolphin,zebra
+        # "potamus", "phant", "key", "phin", "bra"
+        if animal2 == "potamus":
+            animal2 = "Hippopotamus"
+        elif animal2 == "phant":
+            anima2 = "Elephant"
+        elif animal2 == "key":
+            anima2 = "Monkey"
+        elif animal2 == "phin":
+            anima2 = "Dolphin"
+        elif animal2 == "bra":
+            anima2 = "Zebra"
+
+            
+
+        return render_template('index.html', title="Random Animal Name Generator", randanimal=randanimal, animal2=animal2, animal1=animal1, form=form)
+
+        
 
     return render_template('index.html', title="Random Animal Name Generator", randanimal=randanimal, animal2=animal2, animal1=animal1, form=form)
--- service-4/application/routes.py
+++ service-4/application/routes.py
@@ -1,19 +1,19 @@
 # from flask import render_template, Response
 # import random
 
-# from application import app
+from application import app
 
 
-# @app.route('/', methods = ['GET', 'POST'])
-# @app.route('/home', methods = ['GET', 'POST'])
-# def genanimal3():
-#     #hippopotamus,elephant,monkey,dolphin,zebra
-#     G2animals = ["Hippo", "Eleph", "Monk", "Dol", "Zeb"]
-#     G2randAnimal = random.choice(G2animals)
-#     #lion,dog,cat,cow,sheep
-#     G1animals = ["on", "og", "at", "ow", "eep"]
-#     G1randAnimal = random.choice(G1animals)
+@app.route('/', methods = ['GET', 'POST'])
+@app.route('/home', methods = ['GET', 'POST'])
+def genanimal3():
+    #hippopotamus,elephant,monkey,dolphin,zebra
+    G2animals = ["Hippo", "Eleph", "Monk", "Dol", "Zeb"]
+    G2randAnimal = random.choice(G2animals)
+    #lion,dog,cat,cow,sheep
+    G1animals = ["on", "og", "at", "ow", "eep"]
+    G1randAnimal = random.choice(G1animals)
 
-#     service4animal = G1randAnimal + G2randAnimal
+    service4animal = G1randAnimal + G2randAnimal
     
-#     return render_template('index.html', title="Random Animal Name Generator", service4animal=service4animal)
+    return render_template('index.html', title="Random Animal Name Generator", service4animal=service4animal)
