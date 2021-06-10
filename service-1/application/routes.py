26fda5fa3cfef9c29bcbe0526844e10bf34926aa
mattc <mattcoleman1995@gmail.com>
2021-06-10 18:04:34 +0000

    Fully working App

--- service-1/application/routes.py
+++ service-1/application/routes.py
@@ -12,9 +12,8 @@ from application.models import AnimalNames
 @app.route('/home', methods = ['GET', 'POST'])
 def index():
 
-    animal1 = "animal 1"
-    animal2 = "animal 2"
-    randanimal = "new animal"
+    animal1 = ""
+    animal2 = ""
 
     form = GenerateAnimal()
 
@@ -22,6 +21,7 @@ def index():
         return render_template('index.html', title="Random Animal Name Generator", form=form)
     
     if form.validate_on_submit():
+        userlanding = False
         get_animal1 = requests.get('http://service-2:5000/animal1').text
         animal1 = get_animal1
         get_animal2 = requests.get('http://service-3:5000/animal2').text
@@ -30,38 +30,37 @@ def index():
 
         newanimal = makeanimalname.text
 
-        #randanimal = animal1 + animal2
+
+
         #Lion,  Dog,    Cat,    Cow,    Sheep
         #"Li",  "Do",   "Ca",   "Co",   "She"
-        # if animal1 == "Li":
-        #     animal1 = "Lion"
-        # elif animal1 == "Do":
-        #     animal1 = "Dog"
-        # elif animal1 == "Ca":
-        #     animal1 = "Cat"
-        # elif animal1 == "Co":
-        #     animal1 = "Cow"
-        # elif animal1 == "She":
-        #     animal1 = "Sheep"
+        if animal1 == "Li":
+            animal1 = "Lion"
+        elif animal1 == "Do":
+            animal1 = "Dog"
+        elif animal1 == "Ca":
+            animal1 = "Cat"
+        elif animal1 == "Co":
+            animal1 = "Cow"
+        elif animal1 == "She":
+            animal1 = "Sheep"
         
-        # #hippopotamus,elephant,monkey,dolphin,zebra
-        # # "potamus", "phant", "key", "phin", "bra"
-        # if animal2 == "potamus":
-        #     animal2 = "Hippopotamus"
-        # if animal2 == "phant":
-        #     animal2 = "Elephant"
-        # if animal2 == "key":
-        #     animal2 = "Monkey"
-        # if animal2 == "phin":
-        #     animal2 = "Dolphin"
-        # if animal2 == "bra":
-        #     animal2 = "Zebra"
+        #hippopotamus,elephant,monkey,dolphin,zebra
+        # "potamus", "phant", "key", "phin", "bra"
+        if animal2 == "potamus":
+            animal2 = "Hippopotamus"
+        if animal2 == "phant":
+            animal2 = "Elephant"
+        if animal2 == "key":
+            animal2 = "Monkey"
+        if animal2 == "phin":
+            animal2 = "Dolphin"
+        if animal2 == "bra":
+            animal2 = "Zebra"
         
         db.session.add(AnimalNames(animalname = newanimal))
         db.session.commit()
 
         allanimalnames = AnimalNames.query.order_by(desc(AnimalNames.id)).limit(5).all()
 
-        return render_template('index.html', title="Random Animal Name Generator", newanimal=newanimal, allanimalnames=allanimalnames, form=form)#randanimal=randanimal, animal2=animal2, animal1=animal1, form=form, )
-
-    return render_template('landing-page.html', title="Random Animal Name Generator")#randanimal=randanimal, animal2=animal2, animal1=animal1, form=form)
+        return render_template('index.html', title="Random Animal Name Generator", userlanding=userlanding, newanimal=newanimal, allanimalnames=allanimalnames, animal1=animal1, animal2=animal2, form=form)#randanimal=randanimal, animal2=animal2, animal1=animal1, form=form, )
