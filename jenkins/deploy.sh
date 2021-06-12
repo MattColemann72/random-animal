27a74de8da6f820aaca6e09bbdc538f16f06521c
MattColemann72 <matt@matt.com>
2021-06-12 12:29:52 +0000

    Allow export of some credentials to stack

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -6,5 +6,7 @@ scp -i ~/.ssh/ansible_id_rsa docker-compose.yaml jenkins@10.154.0.24:home/jenkin
 #docekr stack deploy
 ssh -i ~/.ssh/ansible_id_rsa jenkins@10.154.0.24 << EOF
     export DATABASE_URI=${DATABASE_URI}
+    export DOCKER_USERNAME=${DOCKER_USERNAME}
+    export DOCKER_PASSWORD=${DOCKER_PASSWORD}
     docker stack deploy --compose-file docker-compose.yaml random-animal
 EOF
