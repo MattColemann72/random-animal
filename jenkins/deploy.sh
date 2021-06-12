a1baa9a71ce6c698472b6742fffad31d82e4ee9c
MattColemann72 <matt@matt.com>
2021-06-12 11:41:39 +0000

    Adding Deploy stage

---
+++ jenkins/deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#copy over compose.yaml to manager node
+scp -i ~/.ssh/ansible_id_rsa docker-compose.yaml jenkins@10.154.0.24:home/jenkins/docker-compose.yaml
+
+#docekr stack deploy
+ssh -i ~/.ssh/ansible_id_rsa jenkins@10.154.0.24 "docker stack deploy --compose-file docker-compose.yaml random-animal"
