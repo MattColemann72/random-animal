239a348d860d5e2ef00dbe8a0dff964e8852d364
MattColemann72 <matt@matt.com>
2021-06-12 12:27:44 +0000

    Added Deploy functionality

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -4,4 +4,7 @@
 scp -i ~/.ssh/ansible_id_rsa docker-compose.yaml jenkins@10.154.0.24:home/jenkins/docker-compose.yaml
 
 #docekr stack deploy
-ssh -i ~/.ssh/ansible_id_rsa jenkins@10.154.0.24 "docker stack deploy --compose-file docker-compose.yaml random-animal"
+ssh -i ~/.ssh/ansible_id_rsa jenkins@10.154.0.24 << EOF
+    export DATABASE_URI=${DATABASE_URI}
+    docker stack deploy --compose-file docker-compose.yaml random-animal
+EOF
