a860b4c7b5b5d0a7ee284dea40d65c143eeada6f
MattColemann72 <82821511+MattColemann72@users.noreply.github.com>
2021-06-13 19:11:12 +0100

    forgot to save some chaanges

--- service-3/application/routes.py
+++ service-3/application/routes.py
@@ -7,9 +7,9 @@ from application import app
 @app.route('/', methods = ['GET', 'POST'])
 @app.route('/animal2', methods = ['GET', 'POST'])
 def genanimal2():
-    # Giraffe, Lizzard, Rhino, Flamingo, Penguin
+    # Giraffe, Lizzard, Rhino, Flamingo, Penguin, Armadillo
     # raffe, zard, hino, mingo, guin
-    G2animals = ["raffe", "zard", "hino", "mingo", "guin"]
+    G2animals = ["raffe", "zard", "hino", "mingo", "guin", "dillo"]
     G2randAnimal = random.choice(G2animals)
     
     return Response(G2randAnimal, mimetype='text/plain')
